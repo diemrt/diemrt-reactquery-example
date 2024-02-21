d7a4b84bdcaadb297cc32d298a357a0c3e83e10e
Diego Martignoni <diego.martignoni@abenergie.it>
2024-02-21 18:39:04 +0100

    Add login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { QueryClientProvider } from "@tanstack/react-query"
-import queryClient from "./api/utils"
+import { useQuery } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import AppRouter from "./components/AppRouter/AppRouter.component";
 import './index.css'
+import { checkUserSession } from "./api/firebase/firebase.api";
+import WithSkeleton from "./components/WithSkeleton/WithSkeleton.component";
 
 function App() {
+  const AppRouterWithSkeleton = WithSkeleton(AppRouter)
+  const checkUserSessionsQueryOptions = checkUserSession()
+  const { isPending } = useQuery(checkUserSessionsQueryOptions)
   return (
-    <QueryClientProvider client={queryClient}>
-      <AppRouter />
+    <>
+      <AppRouterWithSkeleton isLoading={isPending} />
       <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    </>
   )
 }
 
