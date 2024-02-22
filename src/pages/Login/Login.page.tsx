32e56352b1c0fe613588fb7b728e767cd32da9d5
Diego Martignoni <diego.martignoni@abenergie.it>
2024-02-22 17:28:19 +0100

    Fix login button

--- src/pages/Login/Login.page.tsx
+++ src/pages/Login/Login.page.tsx
@@ -13,15 +13,15 @@ const Login = () => {
     setFocus,
     formState: { errors },
   } = useForm<LoginFormFields>();
-  const mutation = useMutation(loginMutation())
+  const mutation = useMutation(loginMutation());
 
   const onSubmit = (values: LoginFormFields) => {
-    mutation.mutate(values)
-  }
+    mutation.mutate(values);
+  };
 
   useEffect(() => {
-    setFocus("email", { shouldSelect: true })
-  }, [setFocus])
+    setFocus("email", { shouldSelect: true });
+  }, [setFocus]);
 
   return (
     <div className="dark:bg-slate-900 bg-gray-100 flex h-screen items-center py-16">
@@ -82,12 +82,27 @@ const Login = () => {
                   >
                     Hai dimenticato la password?
                   </NavLink>
-                  <button
-                    type="submit"
-                    className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                  >
-                    Accedi
-                  </button>
+                  {mutation.isPending ? (
+                    <button
+                      disabled
+                      type="button"
+                      className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                    >
+                      <span
+                        className="animate-spin inline-block size-4 border-[3px] border-current border-t-transparent text-white rounded-full"
+                        role="status"
+                        aria-label="loading"
+                      />
+                      Accedi
+                    </button>
+                  ) : (
+                    <button
+                      type="submit"
+                      className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                    >
+                      Accedi
+                    </button>
+                  )}
                 </div>
               </form>
             </div>
